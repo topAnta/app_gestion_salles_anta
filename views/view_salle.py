607a55eb93085d4f9528352bb8b19b1c025797e8
topAnta <anta.top@monboreal.ca>
2026-04-19 02:31:31 -0400

    view

--- views/view_salle.py
+++ views/view_salle.py
@@ -1,15 +1,18 @@
 import customtkinter as ctk
 from services.services_salle import ServiceSalle
 from models.salle import Salle
+from tkinter import ttk
+
 
 class ViewSalle(ctk.CTk):
 
     def __init__(self):
         super().__init__()
-        self.service = ServiceSalle()
 
+        self.service = ServiceSalle()
         self.title("Gestion des salles")
 
+        # ENTRY
         self.entry_code = ctk.CTkEntry(self)
         self.entry_code.pack()
 
@@ -22,97 +25,86 @@ class ViewSalle(ctk.CTk):
         self.entry_cap = ctk.CTkEntry(self)
         self.entry_cap.pack()
 
-        btn = ctk.CTkButton(self, text="Ajouter", command=self.ajouter)
-        btn.pack()
+        # BUTTONS
+        ctk.CTkButton(self, text="Ajouter", command=self.ajouter).pack()
+        ctk.CTkButton(self, text="Supprimer", command=self.supprimer).pack()
+        ctk.CTkButton(self, text="Rechercher", command=self.rechercher).pack()
 
+        # TABLE
+        self.cadreList = ctk.CTkFrame(self)
+        self.cadreList.pack(pady=10)
+
+        self.treeList = ttk.Treeview(
+            self.cadreList,
+            columns=("code", "description", "categorie", "capacite"),
+            show="headings"
+        )
+
+        self.treeList.heading("code", text="CODE")
+        self.treeList.heading("description", text="Description")
+        self.treeList.heading("categorie", text="Catégorie")
+        self.treeList.heading("capacite", text="Capacité")
+
+        self.treeList.pack()
+
+        self.lister_salles()
+
+    # ---------------- AJOUTER ----------------
     def ajouter(self):
+        try:
+            cap = int(self.entry_cap.get())
+        except:
+            print("Capacité invalide")
+            return
+
         salle = Salle(
             self.entry_code.get(),
             self.entry_desc.get(),
             self.entry_cat.get(),
-            int(self.entry_cap.get())
+            cap
         )
 
         self.service.ajouter_salle(salle)
+        self.lister_salles()
+        print("Salle ajoutée")
 
-        
-    def ajouter(self):
-    try:
-        cap = int(self.entry_cap.get())
-    except:
-        print("Capacité invalide")
-        return
-
-    salle = Salle(
-        self.entry_code.get(),
-        self.entry_desc.get(),
-        self.entry_cat.get(),
-        cap
-    )
-
-    self.service.ajouter_salle(salle)
-    self.lister_salles()
-    print("Salle ajoutée")
-
+    # ---------------- SUPPRIMER ----------------
     def supprimer(self):
-    code = self.entry_code.get()
+        code = self.entry_code.get()
+        self.service.supprimer_salle(code)
+        self.lister_salles()
+        print("Salle supprimée")
 
-    self.service.supprimer_salle(code)
-    self.lister_salles()
-    print("Salle supprimée")
-
+    # ---------------- RECHERCHER ----------------
     def rechercher(self):
-    code = self.entry_code.get()
+        code = self.entry_code.get()
 
-    salle = self.service.rechercher_salle(code)
-    self.lister_salles()
+        salle = self.service.rechercher_salle(code)
+        self.lister_salles()
 
-    if salle:
-        self.entry_desc.delete(0, "end")
-        self.entry_cat.delete(0, "end")
-        self.entry_cap.delete(0, "end")
+        if salle:
+            self.entry_desc.delete(0, "end")
+            self.entry_cat.delete(0, "end")
+            self.entry_cap.delete(0, "end")
 
-        self.entry_desc.insert(0, salle.description)
-        self.entry_cat.insert(0, salle.categorie)
-        self.entry_cap.insert(0, salle.capacite)
+            self.entry_desc.insert(0, salle.description)
+            self.entry_cat.insert(0, salle.categorie)
+            self.entry_cap.insert(0, salle.capacite)
 
-        print("Salle trouvée")
-    else:
-        print("Salle non trouvée")
+            print("Salle trouvée")
+        else:
+            print("Salle non trouvée")
 
-ctk.CTkButton(self, text="Ajouter", command=self.ajouter).pack()
-ctk.CTkButton(self, text="Modifier", command=self.modifier).pack()
-ctk.CTkButton(self, text="Supprimer", command=self.supprimer).pack()
-ctk.CTkButton(self, text="Rechercher", command=self.rechercher).pack()
+    # ---------------- LISTE ----------------
+    def lister_salles(self):
+        self.treeList.delete(*self.treeList.get_children())
 
-from tkinter import ttk
-self.cadreList = ctk.CTkFrame(self)
-self.cadreList.pack(pady=10)
+        liste = self.service.recuperer_salles()
 
-self.treeList = ttk.Treeview(
-    self.cadreList,
-    columns=("code", "description", "categorie", "capacite"),
-    show="headings")
-
-self.treeList.heading("code", text="CODE")
-self.treeList.heading("description", text="Description")
-self.treeList.heading("categorie", text="Catégorie")
-self.treeList.heading("capacite", text="Capacité")
-
-self.treeList.pack()
-self.lister_salles()
-
-
-def lister_salles(self):
-    self.treeList.delete(*self.treeList.get_children())
-
-    liste = self.service.recuperer_salles()
-    self.lister_salles()
-
-    for s in liste:
-        self.treeList.insert("", "end", values=(
-            s.code,
-            s.description,
-            s.categorie,
-            s.capacite
-        ))
+        for s in liste:
+            self.treeList.insert("", "end", values=(
+                s.code,
+                s.description,
+                s.categorie,
+                s.capacite
+            ))
